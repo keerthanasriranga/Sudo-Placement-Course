d9fa23722855e595e3deaf904cb2be291126b44a
Keerthana Sriranga <keerthanasriranga11@gmail.com>
2018-09-30 22:44:54 +0530

    Create 13.java

---
+++ 13.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		//code
+		Scanner sc = new Scanner(System.in);
+		int cases = sc.nextInt();
+		while(cases-->0){
+		    System.out.println((int)(Math.floor(Math.sqrt(sc.nextLong()))));
+		}
+	}
+}
+
+https://practice.geeksforgeeks.org/problems/number-of-open-doors/0/?ref=self
